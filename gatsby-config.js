8ff55b38d9994612833003d83f56bf51921b4461
John Omar <me@johnomar.com>
2019-01-02 14:39:19 -0800

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-131652906-1`,
       },
     },
     `gatsby-plugin-feed`,
