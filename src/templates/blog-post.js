6b2f61b8fb75b1fb26c215fdfb183e613b9f09a5
John Omar <me@johnomar.com>
2019-01-01 18:12:37 -0800

    async convertkit

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-
+import Helmet, { HelmetProvider } from 'react-helmet-async'
 import Bio from '../components/Bio'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
@@ -14,55 +14,57 @@ class BlogPostTemplate extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title={post.frontmatter.title} description={post.excerpt} />
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
+      <h1>{post.frontmatter.title}</h1>
+      <p
+      style={{
+        ...scale(-1 / 5),
+        display: `block`,
+        marginBottom: rhythm(1),
+        marginTop: rhythm(-1),
+      }}
+      >
+      {post.frontmatter.date}
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <hr
+      style={{
+        marginBottom: rhythm(1),
+      }}
       />
 
-      <div>
+      <HelmetProvider>
+        <Helmet>
         <script async data-uid="ee1163b5a7" src="https://f.convertkit.com/ee1163b5a7/1027cf2a14.js"></script>
-      </div>
+        </Helmet>
+      </HelmetProvider>
       
       <Bio />
 
       <ul
       style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
+        display: `flex`,
+        flexWrap: `wrap`,
+        justifyContent: `space-between`,
+        listStyle: `none`,
+        padding: 0,
+      }}
+      >
+      <li>
+      {previous && (
+        <Link to={previous.fields.slug} rel="prev">
+        ← {previous.frontmatter.title}
+        </Link>
+      )}
+      </li>
+      <li>
+      {next && (
+        <Link to={next.fields.slug} rel="next">
+        {next.frontmatter.title} →
+        </Link>
+      )}
+      </li>
+      </ul>
       </Layout>
     )
   }
