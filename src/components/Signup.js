ad869b4cbd4436dfe32a70475dd428b5087b98a3
John Omar <me@johnomar.com>
2019-01-02 11:48:14 -0800

    refactor

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import scriptLoader from "react-async-script";
 
-export class Signup extends React.Component {
+class Signup extends React.Component {
   componentDidUpdate(prevProps) {
     // convertkit has loaded via async script
     if (!prevProps.signup && this.props.signup) {
@@ -11,17 +11,6 @@ export class Signup extends React.Component {
   render() { return (
     <div data-uid="c9cdd48911" ref={(r) => this._container = r} />
   )}
-}
-
-class Signup extends React.Component {
-  constructor(props) {
-    super(props);
-    this._internalRef = React.createRef();
-  }
-  componentDidMount() {
-    console.log("ComponentNeedsScript's Instance -", this._internalRef.current);
-  }
-  render() { return (<AsyncHoc ref={this._internalRef} />)}
 }
 
 export default scriptLoader("https://f.convertkit.com/c9cdd48911/c7daeee6d4.js", {
